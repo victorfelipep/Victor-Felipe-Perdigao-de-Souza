7e9e8cc42e0d03ad56809f8b01727dfbfd6325e4
victorfelipep <104962423+victorfelipep@users.noreply.github.com>
2022-06-04 15:55:13 -0300

    update 0406 2

--- src/components/deck.js
+++ src/components/deck.js
@@ -13,7 +13,6 @@ const cards = [
   qatarSimulator
 ]
 
-// These two are just helpers, they curate spring data, values that are later being interpolated into css
 const to = (i: number) => ({
   x: 0,
   y: i * -4,
@@ -22,7 +21,7 @@ const to = (i: number) => ({
   delay: i * 100,
 })
 const from = (_i: number) => ({ x: 0, rot: 0, scale: 2, y: -1000 })
-// This is being used down there in the view, it interpolates rotation and scale into a css transform
+
 const trans = (r: number, s: number) =>
   `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
 
@@ -39,29 +38,28 @@ function Cards() {
     if (indexCarta === 2) {
       window.open(
         'https://victorfelipep.github.io/world-cup-simulator/',
-        '_blank' // <- This is what makes it open in a new window.
+        '_blank' 
         )
     }
     if (indexCarta === 1) {
       window.location.reload()
     }
   }
-  const [gone] = useState(() => new Set()) // The set flags all the cards that are flicked out
+  const [gone] = useState(() => new Set()) 
   const [props, api] = useSprings(cards.length, i => ({
     ...to(i),
     from: from(i),
-  })) // Create a bunch of springs using the helpers above
-  // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
+  })) 
   const bind = useDrag(({ args: [index], down, movement: [mx], direction: [xDir], velocity }) => {
-    const trigger = velocity > 0.2 // If you flick hard enough it should trigger the card to fly out
-    const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
-    if (!down && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+    const trigger = velocity > 0.2 
+    const dir = xDir < 0 ? -1 : 1 
+    if (!down && trigger) gone.add(index) 
     api.start(i => {
-      if (index !== i) return // We're only interested in changing spring-data for the current spring
+      if (index !== i) return 
       const isGone = gone.has(index)
-      const x = isGone ? (200 + window.innerWidth) * dir : down ? mx : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
-      const rot = mx / 100 + (isGone ? dir * 10 * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
-      const scale = down ? 1.6 : 1.5 // Active cards lift up a bit
+      const x = isGone ? (200 + window.innerWidth) * dir : down ? mx : 0 
+      const rot = mx / 100 + (isGone ? dir * 10 * velocity : 0)
+      const scale = down ? 1.6 : 1.5
       return {
         x,
         rot,
@@ -77,7 +75,7 @@ function Cards() {
         api.start(i => to(i))
       }, 600)
   })
-  // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
+  
   return (
     <>
       {props.map(({ x, y, rot, scale }, i) => (
